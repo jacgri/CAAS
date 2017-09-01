8a6ee19359e1e0896462d81ec075aedb97755119
Jacqui Grimes <grimesjacqui@gmail.com>
2017-09-01 17:56:42 +0100

    checking the request property to send back in requested format

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express')
 var app = express()
+var exphbs = require('express-handlebars')
 
-var bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.engine('.hbs', exphbs({
+  extname: '.hbs',
+  defaultLayout: 'main'
+}))
+app.set('view engine', '.hbs')
+
+require('./controllers/caasControllers')(app)
 
 app.listen(3000, function () {
   console.log('Listening on port 3000')
--- controllers/caasControllers.js
+++ controllers/caasControllers.js
@@ -1,24 +1,32 @@
 
-function caasControllers(app) {
+function caasControllers (app) {
+  app.get('/nice/:from/:to', function (req, res) {
+    var compliment = {
+      'message': 'You\'re a nice person ' + req.params.to,
+      'from': req.params.from
+    }
 
-  app.get('/nice/:from/:to', function(req, res){
-    res.send ({
-      "message": "You\'re a nice person " + req.params.to, 
-      "from": req.params.from})
+    res.format({
+      html: function () {
+        res.render('home', {compliment})
+      },
+      default: function () {
+        res.send({compliment})
+      }
+    })
   })
 
-  app.get('/like/:from/:to', function(req, res){
-    res.send ({
-      "message": "I really like you " + req.params.to, 
-      "from": req.params.from})
+  app.get('/like/:from/:to', function (req, res) {
+    res.send({
+      'message': 'I really like you ' + req.params.to,
+      'from': req.params.from})
   })
 
-  app.get('/happy/:from/:to', function(req, res){
-    res.send ({
-      "message": req.params.to + " You\'re such a happy person! ", 
-      "from": req.params.from})
+  app.get('/happy/:from/:to', function (req, res) {
+    res.send({
+      'message': req.params.to + ' You\'re such a happy person! ',
+      'from': req.params.from})
   })
-
 }
 
 module.exports = caasControllers
